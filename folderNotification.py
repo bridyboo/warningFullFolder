2ac35985c473014f48911755e91961f177c0ad05
bridyboo <57050319+bridyboo@users.noreply.github.com>
2023-07-19 00:55:37 -0700

    wrapper around make_archive, shutil's implementation was too complicated, mine is simpler. Now class actually zips properly, this is MVP (sort of)

--- folderNotification.py
+++ folderNotification.py
@@ -9,6 +9,22 @@ import time
 import shutil
 import InstallerFolder
 
+
+# Source is the file that will be archived
+# destination is the folder of the zip
+# target is the zip file created
+def make_archive(source,destination, target):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+    base = os.path.basename(target)
+    name = base.split('.')[0]
+    format = base.split('.')[1]
+    archive_from = os.path.dirname(source)
+    archive_to = os.path.basename(source.strip(os.sep))
+    print(source, target, archive_from, archive_to)
+    shutil.make_archive(name, format, archive_from, archive_to)
+    shutil.move('%s.%s' % (name, format), target)
+
 # Global Variable
 #######################################################################################################################
 POSTFIX = r'5.txt'
@@ -48,13 +64,13 @@ class MyService(win32serviceutil.ServiceFramework):
                 with open(base, "r") as file:  # as a service this opens up the python lib path and creates filepath.txt
                     for path in file:
                         path = path.rstrip('\n')  # important because OS reads the \n breaking the script
-                        destination = trimPathName(path)
+                        destination = trimPathName(path)  # destination is same level dir as the target
                         zipFolder(path, destination)
             except FileNotFoundError: # Creates a file if doesn't exist need to create folder as well
                 with open(base, "w"):
                     pass
             # Sleep for 10 seconds
-            time.sleep(600)
+            time.sleep(10) # sleep also affects the 'service kill' command 1:1 look into making a 'if' stop function
 
 # This function walks through the path queried and zips all the files in the folder into an archive folder
 # This will trigger when there's a file that ends with a specific 'postfix' i.e. testfile999 << ends at 999
@@ -62,15 +78,16 @@ def zipFolder(folder, zip_destination):
     for root, dirs, files in os.walk(folder, topdown=False):
         for file in files:
             if file.endswith(POSTFIX):  # modifies end with postfix
-                shutil.make_archive(os.path.join(zip_destination), format='zip', base_dir=folder)
+                make_archive(folder,os.path.dirname(zip_destination), zip_destination)
                 # os.remove(base + file) ? don't know if this will work
 
 # This function trims the dirname & basename to create unique .zip files for each folder in the 'filepath' file
 def trimPathName(folder):
     root = os.path.dirname(folder)
     basename = os.path.basename(folder)
-    destination = os.path.join(root, 'archives', 'archive_' + basename)
+    destination = os.path.join(root, 'archives', 'archive_' + basename + '.zip')
     return destination
+
 
 
 if __name__ == '__main__':
